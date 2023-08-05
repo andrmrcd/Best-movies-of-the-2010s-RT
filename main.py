c92b8fd07ec63a3904508c3961e6d6ceb5241b4e
Andrei <johnandreicm@gmail.com>
2023-08-05 17:52:50 +0800

    remove int conversion for port variable

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 # Read environment variables for default db credentials
 DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = int(os.environ.get('DB_PORT'))
+DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASSWORD')
